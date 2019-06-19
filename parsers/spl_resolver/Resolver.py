66152418a3032d61831593efdc44156b5b221bd7
Pavlin <astarchenkov@ot.ru>
2019-06-19 15:57:56 +0300

    Fix Resolver

--- parsers/spl_resolver/Resolver.py
+++ parsers/spl_resolver/Resolver.py
@@ -3,14 +3,15 @@
 # resolver.py
 import json
 import re
-import uuid
+from hashlib import sha256
 from parsers.spl_to_sparksql.splunk_parser import SPLtoSQL
 
 
 class Resolver:
 
     subsearch_pattern = r'.+\[(.+?)\]'
-    read_pattern = r'[|\[]? ?search (.+?)[|\]]'
+    read_pattern = r'[\[| ]*search (.+?)[|\]]'
+    read_pattern_simple = r'[\[| ]*search (.+)'
 
     query_replacements = {
         "\\[": "&&OPENSQUAREBRACKET",
@@ -24,13 +25,17 @@ class Resolver:
         self.subsearches = {}
 
     def create_subsearch(self, match_object):
-        subsearch_uuid = uuid.uuid4()
+        subsearch_md5 = sha256(match_object.group(0).encode('utf-8')).hexdigest()
         subsearch_query = match_object.group(1)
         for replacement in self.inverted_query_replacements:
             subsearch_query = subsearch_query.replace(replacement, self.inverted_query_replacements[replacement])
-        subsearch_query = re.sub(self.read_pattern, self.create_read_graph, subsearch_query)
-        self.subsearches['subsearch_%s' % subsearch_uuid] = subsearch_query
-        return match_object.group(0).replace('[%s]' % match_object.group(1), 'subsearch=subsearch_%s' % subsearch_uuid)
+
+        subsearch_query_service, sub_count = re.subn(self.read_pattern, self.create_read_graph, subsearch_query)
+        if not sub_count:
+            subsearch_query_service = re.sub(self.read_pattern_simple, self.create_read_graph, subsearch_query)
+
+        self.subsearches['subsearch_%s' % subsearch_md5] = (subsearch_query, subsearch_query_service)
+        return match_object.group(0).replace('[%s]' % match_object.group(1), 'subsearch=subsearch_%s' % subsearch_md5)
 
     @staticmethod
     def create_read_graph(match_object):
@@ -51,7 +56,9 @@ class Resolver:
         for replacement in self.inverted_query_replacements:
             spl = spl.replace(replacement, self.inverted_query_replacements[replacement])
 
-        spl = re.sub(self.read_pattern, self.create_read_graph, spl)
+        spl, sub_count = re.subn(self.read_pattern, self.create_read_graph, spl)
+        if not sub_count:
+            spl = re.sub(self.read_pattern_simple, self.create_read_graph, spl)
 
         return {'search': spl, 'subsearches': self.subsearches}
 
@@ -59,12 +66,12 @@ class Resolver:
 if __name__ == '__main__':
 
     spl1 = """ot ttl=60| search index=main (GET AND 200) OR (POST AND 404) "asd   ertert xbfert"| timechart span=1w max(val) as val | 
-    join val [search index=notmain 200 | table _time, val, _span] | join host [search index=second 400  ]" | table _raw]"""
+    join val [search index=notmain 200 | table _time, val, _span] | join host [search index=second 400] | table _raw]"""
 
     spl2 = """search index=1st | join type=left val [ search index=2nd | stats count by val |
     join type=left val [ search index=3rd | stats max(val) as val, min(val) as minval ] | table 2] | table end"""
 
-    spl3 = """search index=asd  \\[asd\\]| table 3"""
+    spl3 = """search index=asd"""
 
     resolver = Resolver()
     print(resolver.resolve(spl1))
