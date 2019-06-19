bc95cbff6adec7ac764ce705005b090abe6da96c
Pavlin <astarchenkov@ot.ru>
2019-06-19 18:26:04 +0300

    Add resolver to makejob.

--- handlers/makejob.py
+++ handlers/makejob.py
@@ -4,6 +4,8 @@ import logging
 import tornado.web
 import psycopg2
 
+from parsers.spl_resolver.Resolver import Resolver
+
 
 class MakeJob(tornado.web.RequestHandler):
 
@@ -56,6 +58,7 @@ class MakeJob(tornado.web.RequestHandler):
         cur.execute(check_user_role_stm, (username,))
         fetch = cur.fetchone()
         access_flag = False
+        _indexes = []
         if fetch:
             _indexes = fetch[0]
             if '*' in _indexes:
@@ -68,57 +71,58 @@ class MakeJob(tornado.web.RequestHandler):
                 if index_count == len(indexes):
                     access_flag = True
         self.logger.debug('User has a right: %s' % access_flag)
-        return access_flag
+        return access_flag, _indexes
 
     def make_job(self):
         request = self.request.body_arguments
         self.logger.debug(request)
         original_spl = request["original_spl"][0].decode()
-
         username = request["username"][0].decode()
-        splitted_spl = original_spl.split('|')
-        indexes = re.findall(r"index=(\S+)", splitted_spl[2])
+        indexes = re.findall(r"index=(\S+)", original_spl)
+        resolver = Resolver()
+        resolved_spl = resolver.resolve(original_spl)
 
         conn = psycopg2.connect(**self.db_conf)
         cur = conn.cursor()
+        access_flag, indexes = self.user_have_right(username, indexes, cur)
 
-        if self.user_have_right(username, indexes, cur):
+        if access_flag:
 
             cache_ttl = int(request['cache_ttl'][0])
             tws = int(float(request['tws'][0]))
             twf = int(float(request['twf'][0]))
-            cache_id, creating_date = self.check_cache(cache_ttl, original_spl, tws, twf, cur)
 
-            if cache_id is None:
-                self.logger.debug('No cache')
+            searches = []
+            for search in resolved_spl['subsearches'].values():
+                searches.append(search)
 
-                fields = re.findall(r"\\| ?fields (\S+)+", original_spl)
-                filters = list(filter(
-                    lambda x: 'index' not in x and 'AND' not in x and x, splitted_spl[2].strip().split(' ')[1:]
-                ))
-                calculation = splitted_spl[4 if fields else 3:-1]
+            searches.append(resolved_spl['search'])
 
-                if self.validate():
+            for search in searches:
 
-                    job_id, creating_date = self.check_running(original_spl, tws, twf, cur)
-                    self.logger.debug('Running job_id: %s, creating_date: %s' % (job_id, creating_date))
-                    if job_id is None:
-                        make_job_statement = 'INSERT INTO splqueries \
-                        (original_spl, indexes, fields, filters, tws, twf, calculation, cache_ttl) \
-                        VALUES (%s,%s,%s,%s,%s,%s,%s,%s) RETURNING id, extract(epoch from creating_date);'
-                        self.logger.info(make_job_statement % (original_spl, indexes, fields, filters, tws, twf,
-                                                               calculation, cache_ttl))
-                        cur.execute(make_job_statement, (
-                            original_spl, indexes, fields, filters, tws, twf, calculation, cache_ttl
-                        ))
-                        job_id, creating_date = cur.fetchone()
-                        conn.commit()
+                cache_id, creating_date = self.check_cache(cache_ttl, search['original_spl'], tws, twf, cur)
 
-                    response = {"_time": creating_date, "status": "success", "job_id": job_id}
+                if cache_id is None:
+                    self.logger.debug('No cache')
+
+                    if self.validate():
+
+                        job_id, creating_date = self.check_running(original_spl, tws, twf, cur)
+                        self.logger.debug('Running job_id: %s, creating_date: %s' % (job_id, creating_date))
+                        if job_id is None:
+                            make_job_statement = 'INSERT INTO splqueries \
+                            (original_spl, service_spl, tws, twf, cache_ttl, username) \
+                            VALUES (%s,%s,%s,%s,%s,%s) RETURNING id, extract(epoch from creating_date);'
+                            self.logger.info(make_job_statement % (search[0], search[1], tws, twf, cache_ttl, username))
+                            cur.execute(make_job_statement, (search[0], search[1], tws, twf, cache_ttl, username))
+                            job_id, creating_date = cur.fetchone()
+                            conn.commit()
+
+                        response = {"_time": creating_date, "status": "success", "job_id": job_id}
+                    else:
+                        response = {"status": "fail", "error": "Validation failed"}
                 else:
-                    response = {"status": "fail", "error": "Validation failed"}
-            else:
-                response = {"_time": creating_date, "status": "success", "job_id": cache_id}
+                    response = {"_time": creating_date, "status": "success", "job_id": cache_id}
 
         else:
             response = {"status": "fail", "error": "User has no access to index"}
--- parsers/spl_resolver/Resolver.py
+++ parsers/spl_resolver/Resolver.py
@@ -10,8 +10,8 @@ from parsers.spl_to_sparksql.splunk_parser import SPLtoSQL
 class Resolver:
 
     subsearch_pattern = r'.+\[(.+?)\]'
-    read_pattern = r'[\[| ]*search (.+?)[|\]]'
-    read_pattern_simple = r'[\[| ]*search (.+)'
+    read_pattern_middle = r'\[\s*search (.+?)[\|\]]'
+    read_pattern_start = r'^ *search ([^|]+)'
 
     query_replacements = {
         "\\[": "&&OPENSQUAREBRACKET",
@@ -30,9 +30,8 @@ class Resolver:
         for replacement in self.inverted_query_replacements:
             subsearch_query = subsearch_query.replace(replacement, self.inverted_query_replacements[replacement])
 
-        subsearch_query_service, sub_count = re.subn(self.read_pattern, self.create_read_graph, subsearch_query)
-        if not sub_count:
-            subsearch_query_service = re.sub(self.read_pattern_simple, self.create_read_graph, subsearch_query)
+        subsearch_query_service = re.sub(self.read_pattern_middle, self.create_read_graph, subsearch_query)
+        subsearch_query_service = re.sub(self.read_pattern_start, self.create_read_graph, subsearch_query_service)
 
         self.subsearches['subsearch_%s' % subsearch_md5] = (subsearch_query, subsearch_query_service)
         return match_object.group(0).replace('[%s]' % match_object.group(1), 'subsearch=subsearch_%s' % subsearch_md5)
@@ -41,7 +40,7 @@ class Resolver:
     def create_read_graph(match_object):
         query = match_object.group(1)
         graph = SPLtoSQL.parse(query)
-        return '| read %s |' % json.dumps(graph)
+        return '| read %s' % json.dumps(graph)
 
     def resolve(self, spl):
         spl = spl.replace('\n', ' ')
@@ -52,26 +51,29 @@ class Resolver:
         while _spl[1]:
             _spl = re.subn(self.subsearch_pattern, self.create_subsearch, _spl[0])
 
-        spl = _spl[0]
+        _spl = _spl[0]
+
         for replacement in self.inverted_query_replacements:
-            spl = spl.replace(replacement, self.inverted_query_replacements[replacement])
+            _spl = _spl.replace(replacement, self.inverted_query_replacements[replacement])
 
-        spl, sub_count = re.subn(self.read_pattern, self.create_read_graph, spl)
-        if not sub_count:
-            spl = re.sub(self.read_pattern_simple, self.create_read_graph, spl)
+        _spl = re.sub(self.read_pattern_middle, self.create_read_graph, _spl)
+        _spl = re.sub(self.read_pattern_start, self.create_read_graph, _spl)
 
-        return {'search': spl, 'subsearches': self.subsearches}
+        return {'search': (spl, _spl), 'subsearches': self.subsearches}
 
 
 if __name__ == '__main__':
 
-    spl1 = """ot ttl=60| search index=main (GET AND 200) OR (POST AND 404) "asd   ertert xbfert"| timechart span=1w max(val) as val | 
+    spl1 = """search index=main (GET AND 200) OR (POST AND 404) "asd   ertert xbfert"| timechart span=1w max(val) as val | 
     join val [search index=notmain 200 | table _time, val, _span] | join host [search index=second 400] | table _raw]"""
 
-    spl2 = """search index=1st | join type=left val [ search index=2nd | stats count by val |
+    spl2 = """search index=1st | search hua|  join type=left val [ search index=2nd | stats count by val |
     join type=left val [ search index=3rd | stats max(val) as val, min(val) as minval ] | table 2] | table end"""
 
     spl3 = """search index=asd"""
+
+    spl4 = """|makeresults |search index=main (GET AND 200) OR (POST AND 404) "asd   ertert xbfert"| timechart span=1w max(val) as val | 
+    join val [search index=notmain 200 | table _time, val, _span] | join host [search index=second 400] | table _raw]"""
 
     resolver = Resolver()
     print(resolver.resolve(spl1))
@@ -82,3 +84,5 @@ if __name__ == '__main__':
     resolver = Resolver()
     print(resolver.resolve(spl3))
 
+    resolver = Resolver()
+    print(resolver.resolve(spl4))
