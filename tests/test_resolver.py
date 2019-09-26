8f27318c572d9033b7dea376ac3308456a5e39eb
Pavlin <astarchenkov@ot.ru>
2019-09-26 12:12:09 +0300

    Fix tests

--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -6,7 +6,7 @@ class TestResolver(unittest.TestCase):
 
     def setUp(self) -> None:
         self.maxDiff = None
-        self.resolver = Resolver.Resolver(['index1', 'index2'], 0, 0)
+        self.resolver = Resolver.Resolver(['main', 'main1', 'main2'], 0, 0)
 
     def test_read_some_empty(self):
         spl = """search index=main2 SUCCESS host="h1 bla" OR host="" OR host=h3"""
@@ -41,8 +41,8 @@ class TestResolver(unittest.TestCase):
         self.assertDictEqual(result, target)
 
     def test_subsearche_general(self):
-        spl = """search index=main FAIL | join host [search index=main2 SUCCESS | stats count by host]"""
-        target = {'search': ('search index=main FAIL | join host [search index=main2 SUCCESS | stats count by host]', '| read {"main": {"query": "(_raw like \'%FAIL%\')", "tws": 0, "twf": 0}}| join host subsearch=subsearch_1ae3636e1888e78d8be6893c1e7d569b9289d145bdc8e5d33cdc50aa5bf097e7'), 'subsearches': {'subsearch_1ae3636e1888e78d8be6893c1e7d569b9289d145bdc8e5d33cdc50aa5bf097e7': ('search index=main2 SUCCESS | stats count by host', '| read {"main2": {"query": "(_raw like \'%SUCCESS%\')", "tws": 0, "twf": 0}}| stats count by host')}}
+        spl = """search index=main1 FAIL | join host [search index=main2 SUCCESS | stats count by host]"""
+        target = {'search': ('search index=main1 FAIL | join host [search index=main2 SUCCESS | stats count by host]', '| read {"main1": {"query": "(_raw like \'%FAIL%\')", "tws": 0, "twf": 0}}| join host subsearch=subsearch_1ae3636e1888e78d8be6893c1e7d569b9289d145bdc8e5d33cdc50aa5bf097e7'), 'subsearches': {'subsearch_1ae3636e1888e78d8be6893c1e7d569b9289d145bdc8e5d33cdc50aa5bf097e7': ('search index=main2 SUCCESS | stats count by host', '| read {"main2": {"query": "(_raw like \'%SUCCESS%\')", "tws": 0, "twf": 0}}| stats count by host')}}
         result = self.resolver.resolve(spl)
         print('result', result)
         print('target', target)
@@ -65,17 +65,33 @@ class TestResolver(unittest.TestCase):
         self.assertDictEqual(result, target)
 
     def test_subsearch_with_otoutputlookup(self):
-        spl = """search index=test_index | otoutputlookup testoutputlookup.csv"""
-        target = {'search': ('search index=test_index | otoutputlookup testoutputlookup.csv', '| read {"test_index": {"query": "", "tws": 0, "twf": 0}}| otoutputlookup testoutputlookup.csv'), 'subsearches': {}}
+        spl = """search index=main | otoutputlookup testoutputlookup.csv"""
+        target = {'search': ('search index=main | otoutputlookup testoutputlookup.csv', '| read {"main": {"query": "", "tws": 0, "twf": 0}}| otoutputlookup testoutputlookup.csv'), 'subsearches': {}}
         result = self.resolver.resolve(spl)
         print('result', result)
         print('target', target)
         self.assertDictEqual(result, target)
 
     def test_subsearch_with_append(self):
-        spl = """search index=test_index | table _time, serialField, random_Field, WordField, junkField| append [search index=test_index junkField="word"]"""
-        target = {'search': ('search index=test_index | table _time, serialField, random_Field, WordField, junkField| append [search index=test_index junkField="word"]', '| read {"test_index": {"query": "", "tws": 0, "twf": 0}}| table _time, serialField, random_Field, WordField, junkField| append subsearch=subsearch_110c2b2fb62279fbc26900d477d7a9ca460a566213c797503e701020e366134f'), 'subsearches': {'subsearch_110c2b2fb62279fbc26900d477d7a9ca460a566213c797503e701020e366134f': ('search index=test_index junkField="word"', '| read {"test_index": {"query": "junkField=\\"word\\"", "tws": 0, "twf": 0}}')}}
+        spl = """search index=main | table _time, serialField, random_Field, WordField, junkField| append [search index=main junkField="word"]"""
+        target = {'search': ('search index=main | table _time, serialField, random_Field, WordField, junkField| append [search index=main junkField="word"]', '| read {"main": {"query": "", "tws": 0, "twf": 0}}| table _time, serialField, random_Field, WordField, junkField| append subsearch=subsearch_33bbbb794d2b8f066c0479361a5caeb24e3553ebdbccfe2aaf278c158b7fdabb'), 'subsearches': {'subsearch_33bbbb794d2b8f066c0479361a5caeb24e3553ebdbccfe2aaf278c158b7fdabb': ('search index=main junkField="word"', '| read {"main": {"query": "junkField=\\"word\\"", "tws": 0, "twf": 0}}')}}
         result = self.resolver.resolve(spl)
         print('result', result)
         print('target', target)
         self.assertDictEqual(result, target)
+
+    def test_read_several_indexes(self):
+        spl = """search index=main1 OR index=main2 SUCCESS"""
+        target = {'search': ('search index=main1 OR index=main2 SUCCESS', '| read {"main1": {"query": "(_raw like \'%SUCCESS%\')", "tws": 0, "twf": 0}, "main2": {"query": "(_raw like \'%SUCCESS%\')", "tws": 0, "twf": 0}}'), 'subsearches': {}}
+        result = self.resolver.resolve(spl)
+        print('result', result)
+        print('target', target)
+        self.assertDictEqual(target, result)
+
+    def test_read_indexes_with_wildcards(self):
+        spl = """search index=main* SUCCESS"""
+        target = {'search': ('search index=main* SUCCESS', '| read {"main": {"query": "(_raw like \'%SUCCESS%\')", "tws": 0, "twf": 0}, "main1": {"query": "(_raw like \'%SUCCESS%\')", "tws": 0, "twf": 0}, "main2": {"query": "(_raw like \'%SUCCESS%\')", "tws": 0, "twf": 0}}'), 'subsearches': {}}
+        result = self.resolver.resolve(spl)
+        print('result', result)
+        print('target', target)
+        self.assertDictEqual(target, result)
