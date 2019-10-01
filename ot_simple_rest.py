d53bf38bd0cff46f9959fb8a089936af61797392
Pavlin <astarchenkov@ot.ru>
2019-10-01 18:19:08 +0300

    Release 0.11.0

--- ot_simple_rest.py
+++ ot_simple_rest.py
@@ -21,7 +21,7 @@ __author__ = "Andrey Starchenkov"
 __copyright__ = "Copyright 2019, Open Technologies 98"
 __credits__ = []
 __license__ = ""
-__version__ = "0.10.9"
+__version__ = "0.11.0"
 __maintainer__ = "Andrey Starchenkov"
 __email__ = "astarchenkov@ot.ru"
 __status__ = "Development"
@@ -64,6 +64,7 @@ def main():
     db_conf = dict(config['db_conf'])
     mem_conf = dict(config['mem_conf'])
     disp_conf = dict(config['dispatcher'])
+    resolver_conf = dict(config['resolver'])
 
     # # # # # # # # # # # # # # # # # # # # # # # # # #
 
@@ -74,7 +75,7 @@ def main():
     # Set TORNADO application with custom handlers.
     application = tornado.web.Application([
         (r'/ping', PingPong),
-        (r'/makejob', MakeJob, {"db_conf": db_conf}),
+        (r'/makejob', MakeJob, {"db_conf": db_conf, "resolver_conf": resolver_conf}),
         (r'/loadjob', LoadJob, {"db_conf": db_conf, "mem_conf": mem_conf, "disp_conf": disp_conf}),
         (r'/otrest', SaveOtRest, {"db_conf": db_conf, "mem_conf": mem_conf}),
         (r'/makerolemodel', MakeRoleModel, {"db_conf": db_conf}),
