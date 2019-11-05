c7a7b23219f1256b2ba58a6d783855ab4ddcfdc0
astarchenkov <astarchenkov@ot.ru>
2019-11-05 20:03:49 +0300

    Refactored spl_to_sparksql.

--- parsers/spl_to_sparksql/splunk_parser.py
+++ parsers/spl_to_sparksql/splunk_parser.py
@@ -8,7 +8,7 @@ from parsers.spl_to_sparksql.internal.expressions.baseEvalExpression import Base
 class SPLtoSQL:
     @staticmethod
     def parse_read(spl, av_indexes, tws, twf):
-        ''' Function for parsing read request.
+        """ Function for parsing read request.
         Returns JSON dictionary with index keys and SQL query string
 
         Arguments:
@@ -16,11 +16,11 @@ class SPLtoSQL:
         av_indexes(list): List of all possible indexes
         tws(int): Time to search from
         twf(int): Time to search to
-        
-        '''
+
+        """
 
         # Remove time from SPL and save start time and end time values in tws and twf
-        (spl_time, _tws, _twf) = Timerange.removetime(spl, tws, twf)
+        (_, _tws, _twf) = Timerange.removetime(spl, tws, twf)
         indices_list = []
         fields_list = []
 
@@ -28,37 +28,38 @@ class SPLtoSQL:
         expressions = BaseEvalExpressions(indices_list, fields_list)
 
         # Preprocess SPL string
-        spl = expressions.spl_preprocessing(spl)
+        spl = expressions.spl_preprocess_request(spl)
 
         # Create parglare grammar from SPLGrammar string
-        LALR_grammar = Grammar.from_string(grammar.SPLGrammar)
+        lalr_grammar = Grammar.from_string(grammar.SPLGrammar)
 
-        # Create parglare parser with LALR_grammar
+        # Create parglare parser with lalr_grammar
         # ARGS: Build tree
         # No debug logging
         # Select action in BaseEvalExpressions class for tree node
         # according to expression symbol in SPLGrammar in grammar.py
-        LALR_parser = Parser(LALR_grammar, debug=False, build_tree=True,
-                                 actions={'I' : expressions.remove_index,
-                                          'Q' : expressions.transform_equal,
-                                          'A' : expressions.transform_and,
-                                          'O' : expressions.transform_or,
-                                          'N' : expressions.transform_not,
-                                          'G' : expressions.transform_comma,
-                                          'C' : expressions.transform_comparison,
-                                          'B' : expressions.transform_brackets,
-                                          'V' : expressions.return_value,
-                                          'S' : expressions.return_string,
-                                          'NQ' : expressions.transform_not_equal
-                                          })
+        lalr_parser = Parser(lalr_grammar, debug=False, build_tree=True,
+                             actions={'I': expressions.remove_index,
+                                      'Q': expressions.transform_equal,
+                                      'A': expressions.transform_and,
+                                      'O': expressions.transform_or,
+                                      'N': expressions.transform_not,
+                                      'G': expressions.transform_comma,
+                                      'C': expressions.transform_comparison,
+                                      'B': expressions.transform_brackets,
+                                      'V': expressions.return_value,
+                                      'S': expressions.return_string,
+                                      'NQ': expressions.transform_not_equal
+                                      })
 
         # Build tree
-        tree = LALR_parser.parse(spl)
+        tree = lalr_parser.parse(spl)
 
         # Transform tree to query string
-        query_string = LALR_parser.call_actions(tree)
+        query_string = lalr_parser.call_actions(tree)
 
-        if (query_string == None): query_string = ''
+        if query_string is None:
+            query_string = ''
 
         # Save indices to indices_list from SPL string
         indices_list = expressions.indices_list
@@ -69,29 +70,29 @@ class SPLtoSQL:
         # If index is regular expression,
         # find all indices in av_indexes list
         for index_string in indices_list:
-            if (index_string.find('*') >= 0):
+            if index_string.find('*') >= 0:
                 string = index_string[:index_string.find('*')]
                 for index_name in av_indexes:
-                    if (index_name.find(string) == 0):
+                    if index_name.find(string) == 0:
                         full_indices_list.append(index_name)
             else:
                 full_indices_list.append(index_string)
 
         # Add indices and query strings to map_with_time dictionary
         for index_string in full_indices_list:
-            map_with_time[index_string] = {'query' : query_string, 'tws' : tws, 'twf' : twf}
+            map_with_time[index_string] = {'query': query_string, 'tws': tws, 'twf': twf}
 
         return map_with_time
 
     @staticmethod
     def parse_filter(spl):
-        ''' Function for parsing filter request.
+        """ Function for parsing filter request.
         Returns JSON dictionary with SQL query string
 
         Arguments:
         spl(str): Input SPL request
-        
-        '''
+
+        """
         indices_list = []
         fields_list = []
 
@@ -99,37 +100,38 @@ class SPLtoSQL:
         expressions = BaseEvalExpressions(indices_list, fields_list)
 
         # Preprocess SPL string
-        spl = expressions.spl_preprocessing(spl)
+        spl = expressions.spl_preprocess_request(spl)
 
         # Create parglare grammar from SPLGrammar string
-        LALR_grammar = Grammar.from_string(grammar.SPLGrammar)
+        lalr_grammar = Grammar.from_string(grammar.SPLGrammar)
 
-        # Create parglare parser with LALR_grammar
+        # Create parglare parser with lalr_grammar
         # ARGS: Build tree
         # No debug logging
         # Select action in BaseEvalExpressions class for tree node
         # according to expression symbol in SPLGrammar in grammar.py
-        LALR_parser = Parser(LALR_grammar, debug=False, build_tree=True,
-                                 actions={'I' : expressions.remove_index,
-                                          'Q' : expressions.transform_equal,
-                                          'A' : expressions.transform_and,
-                                          'O' : expressions.transform_or,
-                                          'N' : expressions.transform_not,
-                                          'G' : expressions.transform_comma,
-                                          'C' : expressions.transform_comparison,
-                                          'B' : expressions.transform_brackets,
-                                          'V' : expressions.return_value,
-                                          'S' : expressions.return_string,
-                                          'NQ' : expressions.transform_not_equal
-                                          })
+        lalr_parser = Parser(lalr_grammar, debug=False, build_tree=True,
+                             actions={'I': expressions.remove_index,
+                                      'Q': expressions.transform_equal,
+                                      'A': expressions.transform_and,
+                                      'O': expressions.transform_or,
+                                      'N': expressions.transform_not,
+                                      'G': expressions.transform_comma,
+                                      'C': expressions.transform_comparison,
+                                      'B': expressions.transform_brackets,
+                                      'V': expressions.return_value,
+                                      'S': expressions.return_string,
+                                      'NQ': expressions.transform_not_equal
+                                      })
 
         # Build tree
-        tree = LALR_parser.parse(spl)
+        tree = lalr_parser.parse(spl)
 
         # Transform tree to query string
-        query_string = LALR_parser.call_actions(tree)
-        if (query_string == None): query_string = ''
+        query_string = lalr_parser.call_actions(tree)
+        if query_string is None:
+            query_string = ''
 
         # Create dictionary with key 'query' and value query_string
-        result = {'query' : query_string, 'fields' : fields_list}
+        result = {'query': query_string, 'fields': fields_list}
         return result
