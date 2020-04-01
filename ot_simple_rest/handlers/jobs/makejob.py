a6046a1ca99128361654637b73e30c86bd2e49b8
JetBaget <jetneitrino@gmail.com>
2020-04-01 15:32:22 +0300

    Now with auth and index access checking

--- ot_simple_rest/handlers/jobs/makejob.py
+++ ot_simple_rest/handlers/jobs/makejob.py
@@ -1,18 +1,22 @@
 import logging
+import re
 
 import tornado.web
+import jwt
+
+from handlers.eva.base import BaseHandler
 
 __author__ = "Andrey Starchenkov"
 __copyright__ = "Copyright 2019, Open Technologies 98"
 __credits__ = ["Anton Khromov"]
 __license__ = ""
-__version__ = "0.10.2"
+__version__ = "0.11.2"
 __maintainer__ = "Andrey Starchenkov"
 __email__ = "astarchenkov@ot.ru"
 __status__ = "Development"
 
 
-class MakeJob(tornado.web.RequestHandler):
+class MakeJob(BaseHandler):
     """
     This handler is the beginning of a long way of each SPL/OTL search query in OT.Simple Platform.
     The algorithm of search query's becoming to Dispatcher's Job is next:
@@ -27,15 +31,33 @@ class MakeJob(tornado.web.RequestHandler):
     8. Register new Job in Dispatcher DB.
     """
 
-    def initialize(self, manager):
+    def initialize(self, **kwargs):
         """
         Gets config and init logger.
 
         :param manager: Jobs manager object
         :return:
         """
-        self.jobs_manager = manager
+        super().initialize(kwargs['db_conn_pool'])
+        self.user_conf = kwargs['user_conf']
+        self.check_index_access = False if self.user_conf['check_index_access'] == 'False' else True
+        self.jobs_manager = kwargs['manager']
         self.logger = logging.getLogger('osr')
+
+    def prepare(self):
+        client_token = self.get_cookie('eva_token')
+        if client_token:
+            self.token = client_token
+            try:
+                token_data = self.decode_token(client_token)
+                user_id = token_data['user_id']
+            except (jwt.ExpiredSignatureError, jwt.DecodeError):
+                pass
+            else:
+                self.current_user = user_id
+
+        if not self.current_user:
+            raise tornado.web.HTTPError(401, "unauthorized")
 
     def write_error(self, status_code: int, **kwargs) -> None:
         """Override to implement custom error pages.
@@ -56,12 +78,64 @@ class MakeJob(tornado.web.RequestHandler):
             self.logger.debug(f'Error_msg: {error_msg}')
             self.finish(error_msg)
 
+    def get_original_spl(self):
+        request = self.request.arguments
+        original_spl = request["original_spl"][0].decode()
+        original_spl = re.sub(r"\|\s*ot\s[^|]*\|", "", original_spl)
+        original_spl = re.sub(r"\|\s*simple[^\"]*", "", original_spl)
+        original_spl = original_spl.replace("oteval", "eval")
+        original_spl = original_spl.strip()
+        return original_spl
+
+    def user_has_right(self, indexes):
+        """
+        It checks Role Model if user has access to requested indexes.
+
+        :param indexes: Requested indexes parsed from SPL query.
+        :type indexes: List.
+        :return: Boolean access flag and resolved indexes.
+        """
+        if not indexes:
+            return True, indexes
+
+        accessed_indexes = []
+        user_indexes = self.db.get_indexes_data(user_id=self.current_user,
+                                                names_only=True)
+        access_flag = False
+        if user_indexes:
+            if '*' in user_indexes:
+                access_flag = True
+            else:
+                for index in indexes:
+                    index = index.replace('"', '').replace('\\', '')
+                    for _index in user_indexes:
+                        indexes_from_rm = re.findall(index.replace("*", ".*"), _index)
+                        self.logger.debug("Indexes from rm: %s. Left index: %s. Right index: %s." % (
+                            indexes_from_rm, index, _index
+                        ))
+                        for ifrm in indexes_from_rm:
+                            accessed_indexes.append(ifrm)
+            if accessed_indexes:
+                access_flag = True
+        self.logger.debug(f'User has a right: {access_flag}')
+
+        return access_flag, accessed_indexes
+
     async def post(self):
         """
         It writes response to remote side.
 
         :return:
         """
-        response = await self.jobs_manager.make_job(self.request)
+        original_spl = self.get_original_spl()
+        indexes = re.findall(r"index=(\S+)", original_spl)
+        access_flag, indexes = self.user_has_right(indexes)
+        if not access_flag:
+            return self.write({"status": "fail", "error": "User has no access to index"})
+
+        self.logger.debug(f'User has access. Indexes: {indexes}.')
+        response = await self.jobs_manager.make_job(request=self.request,
+                                                    indexes=indexes)
         self.logger.debug(f'MakeJob RESPONSE: {response}')
         self.write(response)
+
