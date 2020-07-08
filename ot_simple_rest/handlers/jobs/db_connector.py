d3057de479e6309168dfb2bf0f0937b6dbece602
astarchenkov <astarchenkov@isgneuro.com>
2020-07-08 12:46:50 +0300

    Add status 'new' as running status in running check in db_connector.py.

--- ot_simple_rest/handlers/jobs/db_connector.py
+++ ot_simple_rest/handlers/jobs/db_connector.py
@@ -29,7 +29,7 @@ class PostgresConnector(PGConnector):
 
     def check_running(self, *, original_otl, tws, twf, field_extraction, preview):
         job_id = creating_date = None
-        query_str = "SELECT id, extract(epoch from creating_date) FROM OTLQueries WHERE status = 'running' " \
+        query_str = "SELECT id, extract(epoch from creating_date) FROM OTLQueries WHERE status IN ('running', 'new') " \
                     "AND original_otl=%s AND tws=%s AND twf=%s AND field_extraction=%s AND preview=%s;"
         stm_tuple = (original_otl, tws, twf, field_extraction, preview)
         self.logger.info(query_str % stm_tuple)
