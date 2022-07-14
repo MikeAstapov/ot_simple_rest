b2fe60ec07d92d8b8d1e511e83bf4efac3e4eac5
Ilia <isagaidak@isgneuro.com>
2022-07-14 13:27:45 +0300

    add: get one timeline

--- ot_simple_rest/tools/timelines_builder.py
+++ ot_simple_rest/tools/timelines_builder.py
@@ -26,18 +26,24 @@ class TimelinesBuilder:
 
     def __init__(self):
         self.points = 50  # how many points on the timeline
+        self.intervals = {  # intervals for every timeline
+            TimeIntervals.MINUTES: relativedelta(minutes=1),
+            TimeIntervals.HOURS: relativedelta(hours=1),
+            TimeIntervals.DAYS: relativedelta(days=1),
+            TimeIntervals.MONTHS: relativedelta(months=1)
+        }
 
     def fill_in_time(
-            self, timelines: List[List[Dict[str, int]]], old_time: datetime, step: relativedelta, interval: int):
+            self, timeline: List[Dict[str, int]], old_time: datetime, step: relativedelta):
         """fills in time for a single timeline"""
         for i in range(self.points):
-            timelines[interval][i].update(time=old_time.timestamp(), value=0)
+            timeline[i].update(time=old_time.timestamp(), value=0)
             old_time += step
 
-    def fill_in_all_time(self, timelines: List, old_times: Tuple, intervals: Tuple):
+    def fill_in_all_time(self, timelines: List, old_times: Tuple):
         """fills in time for every timeline"""
-        for interval in (TimeIntervals.MINUTES, TimeIntervals.HOURS, TimeIntervals.DAYS, TimeIntervals.MONTHS):
-            self.fill_in_time(timelines, old_times[interval], intervals[interval], interval)
+        for interval in self.intervals.keys():
+            self.fill_in_time(timelines[interval], old_times[interval], self.intervals[interval])
 
     @staticmethod
     def find_index_in_timeline(old_time: datetime, current_time: datetime, interval: int) -> int:
@@ -67,19 +73,45 @@ class TimelinesBuilder:
             fresh_time.replace(hour=0, minute=0, second=0) - relativedelta(days=self.points - 1),
             fresh_time.replace(day=1, hour=0, minute=0, second=0) - relativedelta(months=self.points - 1)
         )
-        intervals = (  # intervals for every timeline
-            relativedelta(minutes=1),
-            relativedelta(hours=1),
-            relativedelta(days=1),
-            relativedelta(months=1)
-        )
-        self.fill_in_all_time(timelines, old_times, intervals)
+        self.fill_in_all_time(timelines, old_times)
         for elem in data:
             elem = datetime.fromtimestamp(elem)
-            # there's no point to check days timeline if months timeline doesn't pass condition
-            for interval in (TimeIntervals.MONTHS, TimeIntervals.DAYS, TimeIntervals.HOURS, TimeIntervals.MINUTES):
+            # list transformation is required cause in python 3.6 dict object doesn't implement __reversed__
+            for interval in reversed(list(self.intervals.keys())):
                 if elem >= old_times[interval]:
                     timelines[interval][self.find_index_in_timeline(old_times[interval], elem, interval)]['value'] += 1
                 else:
-                    break
+                    break  # there's no point to check days timeline if months timeline doesn't pass condition
         return timelines
+
+    def get_one_timeline(self, data: List[int], old_time: datetime, step: int) -> List[Dict[str, int]]:
+        timeline = [{} for _ in range(self.points)]
+        interval = self.intervals[step]
+        self.fill_in_time(timeline, old_time, interval)
+        for elem in data:
+            elem = datetime.fromtimestamp(elem)
+            if elem >= old_time:
+                timeline[self.find_index_in_timeline(old_time, elem, TimeIntervals.MINUTES)]['value'] += 1
+            else:
+                break
+        return timeline
+
+    def get_minutes_timeline(self, data: List[int], fresh_time: int) -> List[Dict[str, int]]:
+        fresh_time = datetime.fromtimestamp(fresh_time)
+        old_time = fresh_time.replace(second=0) - relativedelta(minutes=self.points - 1)  # oldest time
+        return self.get_one_timeline(data, old_time, TimeIntervals.MINUTES)
+
+    def get_hours_timeline(self, data: List[int], fresh_time: int) -> List[Dict[str, int]]:
+        fresh_time = datetime.fromtimestamp(fresh_time)
+        old_time = fresh_time.replace(minute=0, second=0) - relativedelta(hours=self.points - 1)  # oldest time
+        return self.get_one_timeline(data, old_time, TimeIntervals.HOURS)
+
+    def get_days_timeline(self, data: List[int], fresh_time: int) -> List[Dict[str, int]]:
+        fresh_time = datetime.fromtimestamp(fresh_time)
+        old_time = fresh_time.replace(hour=0, minute=0, second=0) - relativedelta(days=self.points - 1)  # oldest time
+        return self.get_one_timeline(data, old_time, TimeIntervals.DAYS)
+
+    def get_months_timeline(self, data: List[int], fresh_time: int) -> List[Dict[str, int]]:
+        fresh_time = datetime.fromtimestamp(fresh_time)
+        old_time = fresh_time.replace(day=1, hour=0, minute=0, second=0) - relativedelta(months=self.points - 1)  # oldest time
+        return self.get_one_timeline(data, old_time, TimeIntervals.MONTHS)
