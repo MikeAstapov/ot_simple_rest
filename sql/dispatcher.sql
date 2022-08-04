a70537611cdeab04cf8b3e0c0f3d1154e1308a6e
Andrey <astarchenkov@isgneuro.com>
2022-08-04 19:33:23 +0300

    Changed original_otl in unique tuple of CachesDL on just created hashed_original_otl column.

--- sql/dispatcher.sql
+++ sql/dispatcher.sql
@@ -26,7 +26,8 @@ CREATE TABLE CachesDL (
     preview BOOLEAN DEFAULT false,
     creating_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     expiring_date TIMESTAMPTZ NOT NULL,
-    UNIQUE(original_otl, tws, twf, field_extraction, preview)
+    hashed_original_otl TEXT NOT NULL,
+    UNIQUE(hashed_original_otl, tws, twf, field_extraction, preview)
 );
 
 CREATE TABLE CachesLock (
